c7a6aa12df530a4c0cfec03ad782e2323d7efa81
Tharushi07 <tharushi.synotec@gmail.com>
2020-03-27 14:16:42 +0530

    #24 Design admission page

---
+++ online-admissions.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+
+    <!-- Mirrored from t.commonsupport.xyz/saratov/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 21 Feb 2020 08:55:19 GMT -->
+    <head>
+        <meta charset="utf-8">
+        <title>Thomas Gall School | Online Admission</title>
+        <!-- Stylesheets -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+
+        <!--Favicon-->
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+        <!-- Responsive -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+        <link href="css/responsive.css" rel="stylesheet">
+        <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+        <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+    </head>
+
+    <body>
+        <div class="page-wrapper">
+
+
+
+            <!-- Main Header-->
+            <?php include './header.php'; ?>
+            <!--End Main Header -->
+
+            <!--Page Title-->
+            <section class="page-title" style="background-image:url(images/background/5.jpg);">
+                <div class="auto-container">
+                    <div class="inner-box">
+                        <h1>Online Admissions</h1>
+                        <ul class="bread-crumb">
+                            <li><a href="index.php">Home</a></li>
+                            <li>Online Admissions</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <!--End Page Title-->
+
+            <!--Contact Section-->
+            <section class="contact-section">
+                <div class="auto-container">
+                    <div class="form-section">
+                        <div class="row clearfix">
+                            <!--Form Column-->
+                            <div class="form-column col-md-8 col-sm-12 col-xs-12">
+                                <!-- contact Form -->
+                                <div class="contact-form">
+                                    <!--contact Form-->
+                                    <form method="post" action="http://t.commonsupport.xyz/saratov/sendemail.php" id="contact-form">
+
+                                        <div class="form-group">
+                                            <input type="text" name="fullname" placeholder="Name in Full *" required>
+                                        </div>                                     
+
+                                        <div class="form-group">
+                                            <input type="text" name="nationality" placeholder="Nationality *" required>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <input type="text" name="gender" placeholder="Gender *" required>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <input type="email" name="email" placeholder="Address *" required>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <input type="text" name="contact" placeholder="Contact No *" required>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <button class="theme-btn btn-style-one" type="submit" name="submit-form">Send Application</button>            
+                                        </div>
+
+                                    </form>
+
+                                </div>
+                                <!--End Contact Form -->
+                            </div>
+
+                            <!--Sidebar-->
+                            <div class="sidebar-side col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                                <aside class="sidebar">
+
+                                    <!--Services Category Widget-->
+                                    <div class="sidebar-widget category-widget">
+                                        <div class="sidebar-title">
+                                            <h3>Admission</h3>
+                                        </div>
+                                        <ul>
+                                            <li><a href="arrange-a-visit.php">Arrange a Visit</a></li>
+                                            <li><a href="term-dates.php">Term Dates</a></li>
+                                            <li><a href="years-group-guide.php">Years Group Guide</a></li>
+                                            <li><a href="school-fees.php">School Fees</a></li> 
+                                            <li><a href="application.php">Applications</a></li> 
+                                            <li><a href="online-admissions.php">Online Admissions</a></li> 
+                                        </ul>
+                                    </div>
+
+                                </aside>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <!--End Contact Section-->
+
+
+            <!-- Main footer-->
+            <?php include './footer.php'; ?>
+            <!--End Main footer -->
+
+        </div>
+        <!--End pagewrapper-->
+
+        <!--Scroll to top-->
+        <div class="scroll-to-top scroll-to-target" data-target=".main-header"><span class="icon fa fa-long-arrow-up"></span></div>
+
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.fancybox.pack.js"></script>
+        <script src="js/jquery.fancybox-media.js"></script>
+        <script src="js/appear.js"></script>
+        <script src="js/wow.js"></script>
+        <script src="js/validate.js"></script>
+        <script src="js/script.js"></script>
+
+        <!--Google Map APi Key-->
+        <script src="http://maps.google.com/maps/api/js?key=AIzaSyBKS14AnP3HCIVlUpPKtGp7CbYuMtcXE2o"></script>
+        <script src="js/map-script.js"></script>
+        <!--End Google Map APi-->
+
+    </body>
+
+    <!-- Mirrored from t.commonsupport.xyz/saratov/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 21 Feb 2020 08:55:19 GMT -->
+</html>
